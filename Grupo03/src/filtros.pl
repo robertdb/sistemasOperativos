468ac799a3acd70484e9b65edd4d30abc0cb8ca5
Pablo Repetto <caussabon@gmail.com>
2017-10-29 02:26:55 -0300

    Agrego stubs de filtros y comentarios

---
+++ Grupo03/src/filtros.pl
@@ -0,0 +1,26 @@
+# Todas las subrutinas reciben
+#   $registro (string) valor del registro como se encuentra en el archivo
+#   %filtros (hash) conjunto de filtros en los que buscar
+#
+# Todas las funciones evaluan a verdadero si y solo si el registro
+# cumple con el filtro y debe ser aceptado.
+
+# Usa $filtros{"e"}
+sub filtrarEntidades {
+}
+
+# Usa $filtros{"f"}
+sub filtrarFuentes {
+}
+
+# Usa $filtros{"d"}
+sub filtrarCondicionesDeDistribucion {
+}
+
+# Usa $filtros{"t"}
+sub filtrarTarjetas {
+}
+
+# Usa $filtros{"c"}
+sub filtrarCuentas {
+}
