5b574eb5a7cff50c204c37f3506d99c3615daabf
Pablo Repetto <caussabon@gmail.com>
2017-10-29 16:03:27 -0300

    Acepto 0 en validacion de filtro

--- Grupo03/src/pruebaValidar.pl
+++ Grupo03/src/pruebaValidar.pl
@@ -1,45 +1,40 @@
 validar();
-# c,t,d,e,f 
-# e puede venir con rango 
-sub validar{
+# c,t,d,e,f
+# e puede venir con rango
+sub validar {
+    print "Ingrese el filtro, manera c20,e20-323,f20,t2020\n";
+    print "O ingrese * para TODOS \n";
+    $filtro = <STDIN>;
+    chomp($filtro);
 
-# my ($filtro) = @_;
-  print "Ingrese el filtro, manera c20,e20-323,f20,t2020\n"; 
-  print "O ingrese * para TODOS \n";
-  $filtro = <STDIN>;
-  chomp($filtro);  
-  
+    if ($filtro eq "0") { return $filtro; }
 
- $incorrecto = 1;
-# Para salir del while debe ser 0
- while ( $incorrecto == 1 ){
-  print "$filtro \n";
-   if ($filtro eq "*") {
-       print "paso, es *\n";
-       $incorrecto = 0;
-   } 
-   else{
-       @array=split(',',$filtro);
-       $hayIncorrecto = 0;
-       foreach $cosa (@array){
-        if ( $cosa =~ /^[c,t,d,e,f].*/) {
-          
+    $incorrecto = 1;
+    while ( $incorrecto == 1 ) {
+        print "$filtro \n";
+        if ($filtro eq "*") {
+            print "paso, es *\n";
+            $incorrecto = 0;
         } else {
-          $hayIncorrecto = 1;
- 	  last;
-        }
-       } # fin Foreach
-# Cuando termina el ciclo, si hay uno malo ---> hayIncorrecto = 1 
-          
-       if ($hayIncorrecto eq 1) {
-          print "Error: modo de filtro invalido, reingrese\n";
-          $filtro = <STDIN>;
-          chomp($filtro);
-        } else {
-        $incorrecto = 0;
-        }
+            @array=split(',',$filtro);
+            $hayIncorrecto = 0;
+            foreach $cosa (@array) {
+                if ( $cosa =~ /^[c,t,d,e,f].*/) {
 
-     }  # Fin else
-   } #Fin While
-  return $filtro;
-} #Fin funcion
+                } else {
+                $hayIncorrecto = 1;
+                last;
+                }
+            }
+
+            if ($hayIncorrecto eq 1) {
+                print "Error: modo de filtro invalido, reingrese\n";
+                $filtro = <STDIN>;
+                chomp($filtro);
+            } else {
+                $incorrecto = 0;
+            }
+        }
+    }
+    return $filtro;
+}
