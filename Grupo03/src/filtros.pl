96857a9db61cabe30c474a10e322e8d3fd7e99f7
Pablo Repetto <caussabon@gmail.com>
2017-10-29 03:46:47 -0300

    filtro por entidades

--- Grupo03/src/filtros.pl
+++ Grupo03/src/filtros.pl
@@ -1,12 +1,26 @@
 # Todas las subrutinas reciben
 #   $registro (string) valor del registro como se encuentra en el archivo
-#   %filtros (hash) conjunto de filtros en los que buscar
+#   \%filtros (hash) conjunto de filtros en los que buscar
 #
 # Todas las funciones evaluan a verdadero si y solo si el registro
 # cumple con el filtro y debe ser aceptado.
 
 # Usa $filtros{"e"}
 sub filtrarEntidades {
+    my @reg = split(/;/, shift @_);
+    my $entidad = @reg[21];
+
+    my %filtros = %{shift @_};
+
+    if (! exists $filtros{"e"}) { return 1; }
+
+    if (!( $filtros{"e"} =~ /^(...)(-(...))?$/ )) { return 0; }
+
+    if ($entidad < $1) { return 0; }
+    if ($3 =! undef && $3 < $entidad) { return 0; }
+    elsif ($3 == undef && $entidad != $1) { return 0; }
+
+    return 1;
 }
 
 # Usa $filtros{"f"}
